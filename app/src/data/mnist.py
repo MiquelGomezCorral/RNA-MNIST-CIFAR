4f959979840fdfd3c9aa2007821bc68826244c20
Miquel Gómez <miquelgc2003@gmail.com>
2025-11-25 16:25:04 +0100

    cleaner

--- app/src/data/mnist.py
+++ app/src/data/mnist.py
@@ -29,7 +29,7 @@ class MNIST_dataset(Dataset):
         image_tensor = self.from_pil_to_tensor(image)
 
         # data augmentation
-        image_tensor = data_augmentation(image_tensor, self.partition, self.CONFIG)
+        image_tensor = self.data_augmentation(image_tensor, self.partition)
 
         # NOTE: net expect a 784 size vector and our dataset 
         # provide 1x28x28 (channels, height, width) -> 784
@@ -41,6 +41,31 @@ class MNIST_dataset(Dataset):
         label = F.one_hot(label, num_classes=10).float()
 
         return {"img": image_tensor, "label": label}
+
+        
+    def data_augmentation(self, image_tensor, partition):
+        # Apply augmentation only to trainwith 50% probability
+        if partition == "test" or torch.rand(1).item() > self.CONFIG.aug_prob: return image_tensor
+    
+        angle = torch.FloatTensor(1).uniform_(-self.CONFIG.angle_range, self.CONFIG.angle_range).item()
+        
+        translate_x = torch.FloatTensor(1).uniform_(-self.CONFIG.translate_range, self.CONFIG.translate_range).item() * self.CONFIG.image_size  
+        translate_y = torch.FloatTensor(1).uniform_(-self.CONFIG.translate_range, self.CONFIG.translate_range).item() * self.CONFIG.image_size  
+        
+        scale = torch.FloatTensor(1).uniform_(self.CONFIG.scale_range[0], self.CONFIG.scale_range[1]).item()
+        
+        shear = torch.FloatTensor(1).uniform_(-self.CONFIG.shear_range, self.CONFIG.shear_range).item()
+        
+        image_tensor = torchvision.transforms.functional.affine(
+            image_tensor,
+            angle=angle,
+            translate=[translate_x, translate_y],
+            scale=scale,
+            shear=[shear, 0],
+            interpolation=torchvision.transforms.InterpolationMode.BILINEAR
+        )
+
+        return image_tensor
 
 
 
@@ -62,67 +87,3 @@ def load_mnist(CONFIG: Configuration):
     print(" - Test images: ", len(test_set))
 
     return train_dataloader, test_dataloader
-
-
-# def data_augmentation(image_tensor, partition):
-#     if partition == "test": return image_tensor
-
-#     r = torch.rand(1).item()
-#     if r > 0.5:
-#         return image_tensor  # No augmentation
-    
-#     r = torch.rand(1).item()
-#     options = 3
-#     if r < 1 / options:
-#         # Add random noise
-#         noise = torch.randn(image_tensor.size()) * 0.1
-#         image_tensor += noise
-#     # elif r < 2/options:
-#     #     # Random brightness adjustment
-#     #     factor = torch.rand(1).item() * 0.2 + 0.9  # Factor between 0.9 and 1.1
-#     #     image_tensor *= factor
-#     elif r < 2/options:
-#         # Random contrast adjustment
-#         factor = torch.rand(1).item() * 0.2 + 0.9  # Factor between 0.9 and 1.1
-#         mean = torch.mean(image_tensor)
-#         image_tensor = (image_tensor - mean) * factor + mean
-#     elif r < 3/options:
-#         # Random rotation
-#         angles = [-15, -10, -5, 5, 10, 15]
-#         angle = angles[torch.randint(0, len(angles), (1,)).item()]
-#         image_tensor = torchvision.transforms.functional.rotate(image_tensor, angle)
-#     elif r < 4/options:
-#         # using affine rotation
-
-#     image_tensor = torch.clamp(image_tensor, 0., 1.)
-#     return image_tensor
-
-
-def data_augmentation(image_tensor, partition, CONFIG: Configuration):
-    """
-    Apply data augmentation to MNIST images.
-    Equivalent to transforms.RandomAffine + Normalize
-    """
-    # MNIST normalization constants
-    
-    
-    if partition == "train":
-        # Apply augmentation with 50% probability
-        if torch.rand(1).item() > CONFIG.aug_prob:
-            angle = torch.FloatTensor(1).uniform_(-CONFIG.angle_range, CONFIG.angle_range).item()
-            translate_x = torch.FloatTensor(1).uniform_(-CONFIG.translate_range, CONFIG.translate_range).item() * CONFIG.image_size  # 10% of image width
-            translate_y = torch.FloatTensor(1).uniform_(-CONFIG.translate_range, CONFIG.translate_range).item() * CONFIG.image_size  # 10% of image height
-            scale = torch.FloatTensor(1).uniform_(CONFIG.scale_range[0], CONFIG.scale_range[1]).item()
-            shear = torch.FloatTensor(1).uniform_(-CONFIG.shear_range, CONFIG.shear_range).item()
-            
-            image_tensor = torchvision.transforms.functional.affine(
-                image_tensor,
-                angle=angle,
-                translate=[translate_x, translate_y],
-                scale=scale,
-                shear=[shear, 0],
-                interpolation=torchvision.transforms.InterpolationMode.BILINEAR
-            )
-    
-
-    return image_tensor
